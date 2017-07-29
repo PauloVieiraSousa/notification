4511e664aef34cf4ebf17219211ef39ffc0b60d6
erickwendel <erick.workspace@gmail.com>
2017-07-29 14:58:28 -0300

    added pm2 keymetrics

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Every = require('every-moment')
 const MongoJS = require('mongojs')
+const db = mongojs('username:password@example.com/mydb?authSource=authdb')
+const notifications = db.collection('notifications')
 Every(1, 'second', () => {
     console.log('Initial!!')
 
